193816dc5a415ecf958b65350a5ded6190c60d50
jinxiaoyu <whxiaoyu123@gmail.com>
2023-02-14 17:05:30 +0800

    review code for 2.7.x

---
+++ whxiaoyu-uc/whxiaoyu-uc-service/src/test/java/com/whxiaoyu/uc/BaseTest.java
@@ -0,0 +1,21 @@
+package com.whxiaoyu.uc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jinxiaoyu
+ */
+@DisplayName("base")
+public class BaseTest {
+
+
+
+    @Test
+    public void test() {
+
+    }
+
+
+
+}
